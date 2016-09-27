3ca8201ca7a8716cb1420a5277602d3dbe77130e
Mark Brown II <mbrownii@doc.gov>
2016-09-27 10:02:41 -0400

    First draft of PowerPoint

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 console.log('hi');
+
+for (var i = 0; i < array.length; i++) {
+	//DO SOMETHING WITH ARRAY
+}
+
+var beyonceSongs = [
+	'Resentment',
+	'Formation',
+	'Irreplaceable',
+	'Listen',
+	'Countdown'
+];
+
+var numbersArray = [ 3, 6, 15, 16 ];
+
+for(key in obj){
+    // The key is key
+    // The value is obj[key]
+}
+
+var lemonadeAlbum = {
+	"release-date" : "April 23, 2016",
+	"genre" : "R&B",
+	"albumLength" : 45:49,
+	"label" : "Columbia",
+	"amazingSingle" : "Formation",
+	"certification" : "platinum"
+}
+
